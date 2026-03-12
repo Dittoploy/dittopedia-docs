b7f2a901f808efc96244171eec5f058671973864
danphannguyen <77532827+danphannguyen@users.noreply.github.com>
2026-03-12 12:01:03 +0100

    feat: :memo: adding playwright documentation

--- src/content/pipeline/_meta.ts
+++ src/content/pipeline/_meta.ts
@@ -1,6 +1,7 @@
 export default {
   index: "Vue d'ensemble",
   'tests-unitaires': 'Tests unitaires',
+  'tests-e2e': 'Tests E2E',
   jenkins: 'Jenkins',
   sonarqube: 'SonarQube',
   docker: 'Docker & Trivy',
